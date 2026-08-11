d2025370ce9d9ab75fce93aa78ce13119d78d075
dramaniako <wipradnyanabayu@gmail.com>
2026-08-11 22:47:47 +0800

    feat: add multiple pages generation

--- src/main.py
+++ src/main.py
@@ -35,7 +35,8 @@ def copy_content(src: str, dst: str):
             copy_content(item_path, new_destination)
             
 def generate_page(from_path:str, template_path:str, dest_path:str):
-    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    file_path = dest_path.replace("md", "html")
+    print(f"Generating page from {from_path} to {file_path} using {template_path}")
     
     with open(from_path, "r") as markdown_file, open(template_path, "r") as template_file:
         markdown = markdown_file.read()
@@ -47,14 +48,25 @@ def generate_page(from_path:str, template_path:str, dest_path:str):
         markdown_file.close()
         template_file.close()
     
-    os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
 
-    with open(dest_path, "w") as destination_file:
+    with open(file_path, "w") as destination_file:
         destination_file.write(template)
+
+def generate_pages(from_path:str, template_path:str, dest_path:str):
+    contents = os.listdir(from_path)
+    for content in contents:
+        content_path = os.path.join(from_path, content)
+        public_path = os.path.join(dest_path, content)
+        if os.path.isfile(content_path):
+            generate_page(content_path, template_path, public_path)
+        elif os.path.isdir(content_path):
+            generate_pages(content_path, template_path, public_path)
+            
 
 def main():
     copy_content("./static", "./public")
-    generate_page("./content/index.md", "./template.html", "./public/index.html")
+    generate_pages("./content", "./template.html", "./public")
     
 
 
